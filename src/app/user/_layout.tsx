0addfe7acb9552e0e2ce7dd7b45009a721b3f9f1
wingddd <wongtaisin1024@gmail.com>
2026-05-12 08:09:22 +0800

    添加商铺详情

--- src/app/user/_layout.tsx
+++ src/app/user/_layout.tsx
@@ -2,7 +2,7 @@
  * @Author: wingddd wongtaisin1024@gmail.com
  * @Date: 2026-05-11 14:59:25
  * @LastEditors: wingddd wongtaisin1024@gmail.com
- * @LastEditTime: 2026-05-11 16:58:15
+ * @LastEditTime: 2026-05-12 08:09:01
  * @FilePath: \wanWanExpo\src\app\user\_layout.tsx
  * @Description:
  *
@@ -14,10 +14,12 @@ const UserLayout = () => {
   return (
     <Stack
       screenOptions={{
-        headerStyle: { backgroundColor: '#fede2b' }
+        headerStyle: { backgroundColor: '#fede2b' },
+        headerTitleAlign: 'center'
       }}
     >
       <Stack.Screen name="shop/index" options={{ title: '商铺' }} />
+      <Stack.Screen name="shop/edit" options={{ title: '商铺信息' }} />
     </Stack>
   )
 }
---
+++ src/app/user/shop/edit.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const shopDetailRender = () => {
+  return (
+    <View>
+      <Text>商铺详情</Text>
+    </View>
+  )
+}
+
+export default shopDetailRender
